7f56141c0f802113505971709a6c89c7ca45977a
fizziksfool <erbe.joshua@gmail.com>
2025-12-23 22:01:56 -0800

    add specific signals forwards for the worker finished signal.

--- src/model/model.py
+++ src/model/model.py
@@ -5,22 +5,28 @@ from .worker import Worker
 
 
 class Model(QObject):
-    worker_finished_sig = Signal()
+    scan_data_loaded_sig = Signal()
+    create_grid_finished_sig = Signal()
 
     def __init__(self, beam_scan: BeamScan) -> None:
         super().__init__()
         self.bs = beam_scan
         self.thread_pool = QThreadPool()
 
-    @Slot()
-    def worker_finished(self) -> None:
-        self.worker_finished_sig.emit()
-
     def load_scan_data(self) -> None:
         self.worker = Worker(self.bs.load_scan_data)
-        self.worker.signals.finished.connect(self.worker_finished)
+        self.worker.signals.finished.connect(self.load_scan_data_finished)
         self.thread_pool.start(self.worker)
+
+    @Slot()
+    def load_scan_data_finished(self) -> None:
+        self.scan_data_loaded_sig.emit()
 
     def create_grid(self, *args, **kwargs) -> None:
         self.worker = Worker(self.bs.create_grid, *args, **kwargs)
+        self.worker.signals.finished.connect(self.create_grid_finished)
         self.thread_pool.start(self.worker)
+
+    @Slot()
+    def create_grid_finished(self) -> None:
+        self.create_grid_finished_sig.emit()
