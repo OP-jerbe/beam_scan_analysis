5015262a6b11d47fd7e0a1216b6669b9a8563e20
fizziksfool <erbe.joshua@gmail.com>
2025-12-23 21:09:59 -0800

    add the worker_finished_sig Signal.

--- src/model/model.py
+++ src/model/model.py
@@ -1,9 +1,11 @@
-from PySide6.QtCore import QObject
+from PySide6.QtCore import QObject, Signal
 
 from .beam_scan import BeamScan
 
 
 class Model(QObject):
+    worker_finished_sig = Signal()
+
     def __init__(self, beam_scan: BeamScan) -> None:
         super().__init__()
         self.bs = beam_scan
