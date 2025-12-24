50ec45ef512007dc7705803d8a6b3ced4f091268
fizziksfool <erbe.joshua@gmail.com>
2025-12-23 21:56:53 -0800

    Add the WorkerSignals class

--- src/model/worker.py
+++ src/model/worker.py
@@ -1,13 +1,25 @@
-from PySide6.QtCore import QRunnable, Slot
+from PySide6.QtCore import QObject, QRunnable, Signal, Slot
 
 
-class Worker(QRunnable):
+class WorkerSignals(QObject):
+    # This survives even after the Worker is deleted
+    finished = Signal()
+    # You could also add error = Signal(str) or result = Signal(object)
+
+
+class Worker(QRunnable, QObject):
+    finished_sig = Signal()
+
     def __init__(self, fn, *args, **kwargs) -> None:
         super().__init__()
         self.fn = fn
         self.args = args
         self.kwargs = kwargs
+        self.signals = WorkerSignals()  # Create the signaler
 
     @Slot()
     def run(self) -> None:
-        self.fn(*self.args, **self.kwargs)
+        try:
+            self.fn(*self.args, **self.kwargs)
+        finally:
+            self.signals.finished.emit()
