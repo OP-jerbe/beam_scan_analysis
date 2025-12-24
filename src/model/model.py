89bbbfb74814c76817b2bc01ad73c967f6c61f73
fizziksfool <erbe.joshua@gmail.com>
2025-12-23 20:59:47 -0800

    add BeamScan as an argument for the model class.

--- src/model/model.py
+++ src/model/model.py
@@ -1,6 +1,9 @@
 from PySide6.QtCore import QObject
 
+from .beam_scan import BeamScan
+
 
 class Model(QObject):
-    def __init__(self) -> None:
+    def __init__(self, beam_scan: BeamScan) -> None:
         super().__init__()
+        self.bs = beam_scan
