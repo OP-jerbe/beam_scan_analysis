8c9a16bc3ea7daaec64c2edb82aa7a9310161ceb
fizziksfool <erbe.joshua@gmail.com>
2025-12-23 21:28:36 -0800

    add the load_scan_data and create_grid methods ot the Model class.

--- src/model/model.py
+++ src/model/model.py
@@ -1,6 +1,7 @@
-from PySide6.QtCore import QObject, Signal
+from PySide6.QtCore import QObject, QThreadPool, Signal
 
 from .beam_scan import BeamScan
+from .worker import Worker
 
 
 class Model(QObject):
@@ -9,3 +10,12 @@ class Model(QObject):
     def __init__(self, beam_scan: BeamScan) -> None:
         super().__init__()
         self.bs = beam_scan
+        self.thread_pool = QThreadPool()
+
+    def load_scan_data(self) -> None:
+        self.worker = Worker(self.bs.load_scan_data)
+        self.thread_pool.start(self.worker)
+
+    def create_grid(self, *args, **kwargs) -> None:
+        self.worker = Worker(self.bs.create_grid, *args, **kwargs)
+        self.thread_pool.start(self.worker)
