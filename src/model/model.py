407ab1a22291a65ca7d328ab04fb630d32290f39
fizziksfool <erbe.joshua@gmail.com>
2025-12-24 09:29:42 -0800

    change the instance of worker to be local to the method.

--- src/model/model.py
+++ src/model/model.py
@@ -14,18 +14,18 @@ class Model(QObject):
         self.thread_pool = QThreadPool()
 
     def load_scan_data(self) -> None:
-        self.worker = Worker(self.bs.load_scan_data)
-        self.worker.signals.finished.connect(self.load_scan_data_finished)
-        self.thread_pool.start(self.worker)
+        worker = Worker(self.bs.load_scan_data)
+        worker.signals.finished.connect(self.load_scan_data_finished)
+        self.thread_pool.start(worker)
 
     @Slot()
     def load_scan_data_finished(self) -> None:
         self.scan_data_loaded_sig.emit()
 
     def create_grid(self, *args, **kwargs) -> None:
-        self.worker = Worker(self.bs.create_grid, *args, **kwargs)
-        self.worker.signals.finished.connect(self.create_grid_finished)
-        self.thread_pool.start(self.worker)
+        worker = Worker(self.bs.create_grid, *args, **kwargs)
+        worker.signals.finished.connect(self.create_grid_finished)
+        self.thread_pool.start(worker)
 
     @Slot()
     def create_grid_finished(self) -> None:
